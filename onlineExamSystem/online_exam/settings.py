ef901f70b3e01806b437fd35922238a9c4af86cf
coding-geek21 <jayapritha9282@gmail.com>
2022-02-26 22:14:04 +0530

    added files

--- onlineExamSystem/online_exam/settings.py
+++ onlineExamSystem/online_exam/settings.py
@@ -136,6 +136,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-WHITENOISE_USE_FINDERS = True
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
